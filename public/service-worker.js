fb2be0cb1dd6ecf8d83afbd0babbf1df4955ceec
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2022-02-08 19:14:53 +0600

    Add pwa support

---
+++ public/service-worker.js
@@ -0,0 +1,59 @@
+var CACHE_NAME = "binni-pwa-cache";
+var urlsToCache = ["/", "/caterings", "/contact", "/about"];
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request).then(function(response) {
+                // Check if we received a valid response
+                if (!response || response.status !== 200 || response.type !== "basic") {
+                    return response;
+                }
+
+                // IMPORTANT: Clone the response. A response is a stream
+                // and because we want the browser to consume the response
+                // as well as the cache consuming the response, we need
+                // to clone it so we have two streams.
+                var responseToCache = response.clone();
+
+                caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            });
+        })
+    );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+    var cacheWhitelist = ["binni-pwa-cache"];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
